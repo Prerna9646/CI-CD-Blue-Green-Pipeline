6094817718fd63945171800eab6dbd8e810e869f
Prerna <prerna9646@gmail.com>
2025-11-10 23:44:15 +0530

    Updated  app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div style={{textAlign:'center', marginTop:'50px'}}>
-      <h1>Blue/Green CI-CD Simulation</h1>
+      <h1>Blue/Green CI-CD Simulation heloooo</h1>
       <h3>Frontend + Backend Working ✅</h3>
       <p>Version: 1.0.0</p>
     </div>
